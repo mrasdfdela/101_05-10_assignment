5aedc24d4cf491ab8d013edd715d7f66c4dac193
mrasdfdela <jerryhsu830@gmail.com>
2020-04-14 13:22:54 -0400

    find the oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    return people.reduce( (c,p)=>{
+        if ( currentAge(p) > currentAge(c) ) {
+            return p;
+        } else {
+            return c;
+        }
+    }, 
+    {
+        name: 'defaultDate',
+        yearOfBirth: new Date().getFullYear()
+    })
+}
 
+const currentAge = function(p) {
+    p.yearOfDeath == undefined ? death = new Date().getFullYear() : death = p.yearOfDeath
+    return death - p.yearOfBirth
 }
 
 module.exports = findTheOldest
+
+// remember that JS object properties can always be referenced; they are undefined/null if not previously defined 
