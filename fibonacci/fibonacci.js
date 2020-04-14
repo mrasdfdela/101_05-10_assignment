322fd09bc204f337697528cf42923aaf784233ed
mrasdfdela <jerryhsu830@gmail.com>
2020-04-14 12:16:39 -0400

    return nth element of fibonacci sequence

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    Number(num) 
 
+    if (num > 0 && num <= 2) {
+        return 1
+    } else if (num > 2) {
+        arr = [1, 1]
+        for (i=2; i<num; i++) {
+            newNum = arr[i-1] + arr[i-2]
+            arr.push(newNum)
+        }
+        return newNum
+    } else {
+        return "OOPS"
+    }
 }
 
 module.exports = fibonacci
